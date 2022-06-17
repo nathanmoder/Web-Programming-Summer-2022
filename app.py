8ca38fc500d9185fc6833c986c316f39fa72a921
Nathan Moder <nmoder@kent.edu>
2022-06-16 20:05:28 -0400

    a

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from re import U
 from tkinter import YView
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
 @app.route("/")
 def get_index():
     return "<p>This is the Nathan's website on PA.</p>"
+
+@app.route("/hello")
+def get_hello():
+    return render_template('hello.html',name="Nathan")
