cd50ee4b3515951c9eeda584910465fd0247c5db
Nathan Moder <nmoder@kent.edu>
2022-06-23 19:42:48 -0400

    hi

--- app.py
+++ app.py
@@ -1,13 +1,51 @@
 from re import U
 from tkinter import YView
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask import request, redirect, url_for, make_response
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+username = None
 
 @app.route("/")
+@app.route("/home")
 def get_index():
-    return "<p>This is the Nathan's website on PA.</p>"
+    username = request.cookies.get("username", None)
+    if username == None:
+        return redirect(url_for('get_login'))
+    return render_template('home.html', name=username)
 
-@app.route("/hello")
-def get_hello():
-    return render_template('hello.html',name="Nathan")
+@app.route("/login", methods=['GET'])
+def get_login():
+    username = request.cookies.get("username", None)
+    if username != None:
+        return redirect(url_for('get_index'))
+    return render_template('login.html')
+
+@app.route("/login", methods=['POST'])
+def post_login():
+    username = request.form.get("username", None)
+    response = make_response(redirect(url_for('get_index')))
+    response.set_cookie("username", username)
+    return response
+
+@app.route("/register", methods=['GET'])
+def get_register():
+    username = request.cookies.get("username", None)
+    if username != None:
+        return redirect(url_for('get_index'))
+    return render_template('register.html')
+
+@app.route("/register", methods=['POST'])
+def post_register():
+    username = request.form.get("username", None)
+    response = make_response(redirect(url_for('get_index')))
+    response.set_cookie("username", username)
+    return response
+
+@app.route("/logout", methods=['GET'])
+def get_logout():
+    response = make_response(redirect(url_for('get_login')))
+    response.delete_cookie('username')
+    return response
