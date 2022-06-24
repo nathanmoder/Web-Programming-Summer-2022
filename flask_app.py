1acdac77d2beec95e966799b431930df1183d867
nathanmoder <63596741+nathanmoder@users.noreply.github.com>
2022-06-23 20:00:10 -0400

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,51 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
+from re import U
+from tkinter import YView
 from flask import Flask
+from flask import render_template
+from flask import request, redirect, url_for, make_response
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+username = None
 
+@app.route("/")
+@app.route("/home")
+def get_index():
+    username = request.cookies.get("username", None)
+    if username == None:
+        return redirect(url_for('get_login'))
+    return render_template('home.html', name=username)
+
+@app.route("/login", methods=['GET'])
+def get_login():
+    username = request.cookies.get("username", None)
+    if username != None:
+        return redirect(url_for('get_index'))
+    return render_template('login.html')
+
+@app.route("/login", methods=['POST'])
+def post_login():
+    username = request.form.get("username", None)
+    response = make_response(redirect(url_for('get_index')))
+    response.set_cookie("username", username)
+    return response
+
+@app.route("/register", methods=['GET'])
+def get_register():
+    username = request.cookies.get("username", None)
+    if username != None:
+        return redirect(url_for('get_index'))
+    return render_template('register.html')
+
+@app.route("/register", methods=['POST'])
+def post_register():
+    username = request.form.get("username", None)
+    response = make_response(redirect(url_for('get_index')))
+    response.set_cookie("username", username)
+    return response
+
+@app.route("/logout", methods=['GET'])
+def get_logout():
+    response = make_response(redirect(url_for('get_login')))
+    response.delete_cookie('username')
+    return response
