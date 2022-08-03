90ed4667362f22ac64f5f93137ae19e00b922196
Nathan Moder <nmoder@kent.edu>
2022-08-02 23:00:55 -0400

    upload for final project

--- cafeProject/setup.py
+++ cafeProject/setup.py
@@ -21,5 +21,14 @@ menu_items=menu_db.menu_items
 menu_items.delete_many({})
 
 #menu_items: id,size,cal,price, type
-menu_items.insert_one({'id':'coffee','size':'8 oz','cal':'100','price':'2.00','type':'drink'})
 #menu_items.insert_one({'id':'','size':'','cal':'','price':'','type':''})
+menu_items.insert_one({'id':'coffee(black)','size':'8 oz','cal':'100','price':'2.00','type':'drink'})
+menu_items.insert_one({'id':'latte','size':'8 oz','cal':'150','price':'2.50','type':'drink'})
+menu_items.insert_one({'id':'iced macha latte','size':'8 oz','cal':'150','price':'3.50','type':'drink'})
+menu_items.insert_one({'id':'water','size':'8 oz','cal':'0','price':'1.00','type':'drink'})
+menu_items.insert_one({'id':'hot chocolate','size':'8 oz','cal':'300','price':'2.50','type':'drink'})
+menu_items.insert_one({'id':'chocolate chip waffle','size':'3 pieces','cal':'1000','price':'25.00','type':'food'})
+menu_items.insert_one({'id':'dark chocolate croissant','size':'1','cal':'200','price':'4.76','type':'food'})
+menu_items.insert_one({'id':'blueberry muffin','size':'1','cal':'170','price':'2.00','type':'food'})
+menu_items.insert_one({'id':'jelly tart','size':'1','cal':'230','price':'3.00','type':'food'})
+menu_items.insert_one({'id':'vodka','size':'16 oz','cal':'1050','price':'1.00','type':'drink'})
